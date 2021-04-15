5f37594695ee82abaeb63b73b877364a33f8bbb9
Abhay <abhayambekar03@gmail.com>
2021-04-15 16:05:28 +0530

    auth gurad extended with keycloakAuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/Shared/auth.guard';
 
-const routes: Routes = [{ path: 'employee', loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule) }];
+const routes: Routes = [{ path: 'employee', canActivate:[AuthGuard], loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
